878685c0c73cc540a50d4d39c4f4b3714496c0a5
superdeann <superdeann@gmail.com>
2015-04-13 18:50:23 -0700

    Test

---
+++ flagi.h
@@ -0,0 +1,19 @@
+#ifndef _FLAGI_H_
+#define _FLAGI_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+
+int flag_s( char* );
+int flag_n( char* ):
+int flag_n( char* );
+int error_control( int, int, int, int, int, int);
+int added_n (int);
+int added_a (int);
+int added_s (int, int);
+int spr_out (char* );
+int spr_open (char* );
+int spr_in (char* );
+
+#endif
